bfe03a43a169ef3b3b559a12725c55bd922e6475
TheProgrammer1000 <denniskarlsszon1998@gmail.com>
2023-02-01 15:21:23 +0100

    SOLVED!

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // const dataInput = 'czfztznzpnpqpfftcftcchfhjjsqsvvffhwfhhqnhqqmlldpdcdnnfvvmpmzpmmrsrwssqbsbsmmdjdssfqfllclcdczdccppvzvtzznpnfpnnljnnpcphpjprrdhddsgdssbqqghqqmgqgccjffdtdcdmdjdcctltdddvhdhfhnnrffwjfjpjnpnpddhbbrrflrldlqlrlqqwssqsrswrrrmcmrcrfrwfwpfpdfppslsjsjhhjjqhqwwzzhttcvvzrrrnlrlmljlrjrsshwwndnnzwzpzcpcllfrrsjsnjncjcvcllrhrbhhdsdqdpqpffmppdzztggvpvnvwvpvdvqddznzrzppznnpnhhwjjrpjpvvqvpvrvfvjvzjvzjzqzlqqwsqqrsrprqpqmmbhbffbnnlzzhszhznnhzhmzzfhflfccvvbnbtnnlcnndjjcvjjshhssfjfcjjpzpqpmmdqmqmmbbldlwwvdwvddznncwcqwcwdwbdwwgzzzqvqqlqlttbppgtptlldldblbbjcbcttbpbvvhlhggghvvqsshjhjjzqzwwcggsfgsgjjgbjgbbgllhhmqmggmsggzszvvzggdngnznlzlrlprpjpgjgvgppfhhbqqcddvvbhbqqmlmsssbbszzfdzzznssjllwlplttdgtdttsmspmpvpgpspqpdqdppfvpvvmggrwwgswswmmmnppmttqppvttrvrllcmctcqqlvvpwwghhlmhmwhmmpfmfvfwwbgbqqdbqblqqclljcljccnhhghbgbrrnrppchhmpmdmfmvffvqqtnqqfccgcfgcgwgcwggvgzvvbwbgbjbwbcbgbzztbbsbdbvddzrdrjjlqqlslrssjddhbddzbzvbvhhrjhhrtrltljljwljjrnjjjlpjphjphjhjvjgjbbwfwcctgtmtmdttjhthphhchfcfnnqlqhqvvjcclczlzwlzlhzlzdlzlbzbvbmbcmcjcncwncntndtntbbgqqbrbsrssrnnrcrmrlmmrqqddhzdhhnqqvccbmbpbwpbwbrwwwgdgnnhzzhphbhwhwzwhhvhqqnmqnmnjmnjjjjztzvzvbzbbvcvggcdchhhhbfbttmmtsmsgmmqvqdvdrvrqrdqqrhqqpsqqfnntsnnslsggcqqrnrmrfffmgmsmgggrdgdrgddzfzqfqfcqfcfrfwrrrgglwlmlmlgmlmrlljzlzglzztllgnndhdthtwwdrrjnncssdsnszsggdhdpprnnqzzltzlzznlzlflgfgbfggqtggfbbccchdcdrrnwnbnhbnbjjpzjpzzwjwcwggfqfccdzzfddmbddgbgrgllnttrwrjwrrbsrsbbwqwmmlggzqgzqzrqzzffmcmssgjjjcdcssndsdfsfddcffbrrcvccwhhzzlglhhsccvdvjjdldndrrvlrrbwrrbqqcbcjcjpjggngvnndtdvttlrlmmcbcggbtbhthjttdggtvtmmlgmlgmmdwdndmmldlttjlmbtrwfnvmnzggqfqwnppbrnjmpjfppwltpqfstgnnjwmbffgfbjmfhqngnswftgrqvpljsmghszclhhprscrgtmsgbwnbrfhwdsbjrdlhdcqfthwfggpzhjrvmprcjjgrlbmdqczmsltnpnlpldvnsjbdjzbrglpsmbwdhhphcdmjqmssdlgjtmphhjvpjqcdvmdtszldnjtbcffhwrbqpsrsmmsqsbbrnhsrvfhhcmwgcpsfhrfcpvldrwstqtgrcgrqttvdnstlbdblnbtcgdgpbcjdlvwpplsslqdsbpncbwzcmlglbhnrtbwvnbfsgrdlpzszhzmzqtztzgjjjszmjgggdnslqqqrrlwcghpvzwpndbjbhhhpbbtvjvjvjhntqggtmqwdnrtswcchsqpdwnvmfqrgpvbtjpttfhmlgtnsgnpcrhbswfblwrbflqlgtmvzvwprtfntshjcpzshnlnqthvqgpzffsbdbvbhpghwjhqpjwjfbsrzgttsnrpmfrvlwrjrfplrtzpntjbvhdjjfjjzrrvrsrctrtldrsgcgvntgvmtrnqbtmpzvlbwnddbnldbhfcpncqclzqgmbjcnnfdfsmdjdgtmtzbpwqrhrlggrfjhvrrqpjptqdzpwffwnfwmfhfhmcpljmhttbltgltqjhtvbdzgwrppzjwhctnncqthnlrhzgtnffpwnnqqgddjtgtczfmwvbccgvmdfvggdghmssjlflptzrpdncbjcsdlbqjfzmnzdggwdczghfpldsbnvzdjzsbsdnmwptgngmnbqmwbrzqgnqbmfvbzzmsvlswbtvfllqzsjdlvzjmbmlbnljwwjsqzqvfwhzmfdgttfqsdttnjmvspsgvpzwczpvdbvhjjrttghjddrgffqdsmzspnssztjjmwtsrtlrmbqhfbvpzsmnqthssbqrjhvqllrbbpjjllszmnjvzmfsbjtvvvgzrvjwldlbzjdbrsvvgmmjrgsbvtvthjbcqlzqhjqfvbzzvlpmrlmphftwbsvvwnwqfwvsnlvlcdgqjspprmwptcjhbjplzrqlhmvzmbfvwwvgvtqlvgfvzqdprfcjqsnndwqmbhlvfvhdhghtsrhbcrrshlsdtlbwntbcvvjhlcscfcnhbdnngtsccrnfbbqbfggvwnmlfhmqbdqpnzvzcvngcctbrshsdvhstcnwvjgwchzdndgbgjnddrmfgftvvjtdnqhfjmpjcjjtmgztvnwnrjjgmcgcqclnwvclngrtjggrprmfpqcsmdtbswhwrgwgfflqldtpnfdhdltrnhdvqqdsshcrnncdpvbhvlcbjlznjjzsdcjjpqprnzpmvqvrfnhdvhgrsmlcgtbsmrgqqtmblqmqcnmnmbcmmjvnptzbbqmcpwbflhccpfrjvfzmcsttfjtjgrnjbtwbncrsjjgjwdpmvfpqmzqwgjvgvfnpbhgzlzhfsnwmjjjsgcjjcwcfbzrmfzwdmhffqnzscbnfmpnbdbccrsjgqgvftszdbqbstwtwmwfzbtmvdmwvshlcpqnsqfwqlfclmjdjvgszsdblwvnhtwtrjcwsmpgflhdqmndrjvqlhqclmwhscnznmhjtqjlnglpbmgvptjnrpnjlqslssfnbdwlrwrdrtfspsjwzvwzfjpflqgmfnbvzqglrrccsnqtchnhlnrwlwqmqdvwsjmbjbvczvqcgfrrtftcgqsnvzhjlshnlvqmvrjljpsvmdzwqgltmccnplbmplhpvvflhmgfzmhczpwffbzhzhhggzgwtpmlzgrbpfgcnmfdbgcwrbgdpzpwmtwgmjtpmqzzgclmcpjgzdrwqfdwqzzqszrwmbwcqzcjdphqtwlbqmnddsdthgjrlshhmzgrqlhghrbqgzppqbdmmlnthhhmtdcdnbqfvbwdrdgbbpbhtrflwmhwjmzdtwmzhjndbrvbtbqnzbsrrbfdrrdnjltdtsjnpddmqzzbzblzvwctpdhfqgqntczwlgqbvvmhjblztlpptqjscztlrvrglrbsbfdjdfbwjltjcdgmqzgtjqjfgjnnpqgfhsrcggrszhbgfhtjmlgmmvshcpjwmcwcvwhnbjzrzfwtnlsnmdgfpbhvgfrhmhmsmtrnblsmnjclqzfgttrdmvpsvlcvnhmgmltfhffzllfrmdqdpzdmjdmsnmpwbwvrdvwvrcdsfwftfbfvmzjttrqgqlqqdtmcpdpfqfsclcmcdzhjwqrbhmzjjsvpbjzzgfwbjbqmzmtrnflwrqgnbgqnpntqmlngmgcrflgvhpznvrfwzlwrswbdsldfdbhvwvcbwhmpcslhmmbjdzvmgrjwzqqzlrrgddpssqcshzrsvrrzzcbsgvfmrlcgjrcpphctdwtjbmrlnrmrbgsqbjnmnsbjfhszcgdqfflcwvrvnwtvcnhcffvphwmzqpwnwncrbnzrnvjzsrnlzlfdbgztrhvlvpczzqnwsvwvnhgnclhndjsgqdgznjzrtmslmgnjzpj';
  
-const dataInput = `bvwbjplbgvbhsrlpgdmjqwftvncz`
+const dataInput = `nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`
 // 7 stycken 
 
 function SortDataInto4(data) {
@@ -27,58 +27,45 @@ function SortDataInto4(data) {
   return dataFinal
 }
 
-function checkElementExist(mergedArray) {
-  const mergedArrayFinal = mergedArray.slice();
-  console.log('mergedArrayFinal: ', mergedArrayFinal);
+function checkElementExist(firstArray, secoundArray) {
+  console.log('firstArray: ', firstArray);
+  console.log('secoundArray: ', secoundArray);
+  let secoundArrayCounter = 0;
+  let firstArrayCounter = 1
 
-  let dataFinal = []
-  let counter = 0;
-  // För varje 4 element så gruppera 
-  let arrayTemp = []
-  for (let i = 0; i < mergedArrayFinal.length; i++) {
-    if(counter === 4) {
-      dataFinal.push(arrayTemp)
-      arrayTemp = [];
-      counter = 0
+  const firstArrayTemp = firstArray.slice(firstArrayCounter) // 1
+  firstArrayTemp.push(secoundArray[secoundArrayCounter]) // 0
+  // Här får vi första gången!
+
+  // console.log('firstArrayTemp: ', firstArrayTemp);
+
+  for (let i = 0; i < firstArrayTemp.length - 1; i++) {
+    for (let j = i + 1; j < firstArrayTemp.length; j++) {
+      if (firstArrayTemp[i] === firstArrayTemp[j]) {  // While skulle vara bra här
+        console.log("Sant");
+        // const sortedArray = sortedDataArrayTemp[i].concat(sortedDataArrayTemp[i + 1]);
+        // console.log(sortedDataArrayTemp[i], sortedDataArrayTemp[i + 1]);
+        // checkElementExist(sortedDataArrayTemp[i], sortedDataArrayTemp[i + 1])
+      }
     }
-    else if(i === mergedArrayFinal.length - 1) {
-      arrayTemp.push(mergedArrayFinal[i])
-      dataFinal.push(arrayTemp);
-      break;
-    }
-    arrayTemp.push(mergedArrayFinal[i])
-    counter++; 
   }
-
-  console.log('dataFinal: ', dataFinal.length);
-
-  for (let i = 0; i < dataFinal.length; i++) {
-    console.log('dataFinal: ', dataFinal[i]);    
-  }
-
-  // for (let i = 0; i < mergedArrayFinal.length; i++) {
-  //   console.log('mergedArrayFinal[i]: ', mergedArrayFinal[i]  );
-  // }
-  // Alltid index 1 här 
-
-
 }
 
 
 function findingDiffrentGroupOf4(sortedDataArray, data) {
-  const sortedDataArrayTemp = sortedDataArray.slice();  
+  const sortedDataArrayTemp = sortedDataArray.slice(); 
+  // console.log('sortedDataArrayTemp: ', sortedDataArrayTemp); 
   // console.log('sortedDataArrayTemp: ', sortedDataArrayTemp);
   for (let i = 0; i < sortedDataArrayTemp.length; i++) {
-    console.log('sortedDataArrayTemp[i]: ', sortedDataArrayTemp[i]);
-    let counter = 0;
     for (let j = 0; j < sortedDataArrayTemp[i].length - 1; j++) {
       for (let z = j + 1; z < sortedDataArrayTemp[i].length; z++) {
         if (sortedDataArrayTemp[i][j] === sortedDataArrayTemp[i][z]) {  // While skulle vara bra här
-          console.log("Sant");
-          
-          const sortedArray = sortedDataArrayTemp[i].concat(sortedDataArrayTemp[i + 1]);
+          // console.log('sortedDataArrayTemp[i][j]: ', sortedDataArrayTemp[i][j]);
+          // console.log('sortedDataArrayTemp[i][z]: ', sortedDataArrayTemp[i][z]);
+
+          // const sortedArray = sortedDataArrayTemp[i].concat(sortedDataArrayTemp[i + 1]);
           // console.log(sortedDataArrayTemp[i], sortedDataArrayTemp[i + 1]);
-          checkElementExist(sortedArray)
+          // checkElementExist(sortedDataArrayTemp[i], sortedDataArrayTemp[i + 1])
         }
       }
     }
@@ -92,11 +79,41 @@ const solotion = (data) => {
   kolla första arrayn finns det 4 olika bokstäver där?
   Om det inte finns ta index 0 och gör den till 1 och kolla resterande
   */
-  const sortedDataArray = SortDataInto4(data);
+  // const sortedDataArray = SortDataInto4(data);
 
-  console.log('data: ', data);
+  let firstPosIndex = -1;
+  let secoundPosIndex = 3
+  let isSameElements = true;
+
+  
+  while(isSameElements) {
+    firstPosIndex++;
+    secoundPosIndex++;
+
+    let arrayTemp = [...data.slice(firstPosIndex, secoundPosIndex)]  // 0 & 4
+    console.log('arrayTemp: ', arrayTemp);
+
+    let isSame = false;
+    for (let i = 0; i < arrayTemp.length - 1; i++) {
+      for (let j = i + 1; j < arrayTemp.length; j++) {
+        if (arrayTemp[i] === arrayTemp[j]) {  // While skulle vara bra här
+          console.log("Sant");
+          isSame = true
+        }
+      }
+    }
+    if(isSame === false) {
+      console.log("Rätt BREAK!");
+      break;
+    }
+    console.log("HÄÄÄR");
+  }
+
+
+  // console.log('data: ', data);
   // Rätt!
-  findingDiffrentGroupOf4(sortedDataArray, data)
+  // findingDiffrentGroupOf4(sortedDataArray, data)
+  
 
   
 
